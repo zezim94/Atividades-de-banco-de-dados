aae49bf1ea0cebb18a1b72bc37b6f78cf7d96dc1
Andelson <andelsonascimento@gmail.com>
2026-05-11 17:15:04 -0300

    atividade finalizada

--- ATV-UC08-008.sql
+++ ATV-UC08-008.sql
@@ -35,7 +35,7 @@ select min(a.`valorAPagar`) as 'Menor valor pago' from aluguel a;
 select avg(a.`valorAPagar`) as 'Média de valor pago' from aluguel a; 
 
 /*10. Calcular o total arrecadado com aluguéis até o dia de hoje.*/
-select sum(a.`valorPago`) as 'Total arrecadado' from aluguel a;
+select sum(ae.`valorItem`) as 'Total arrecadado' from aluguelequipamento ae;
 
 /*11. Listar o nome dos equipamentos com quantidade em estoque maior que 40*/
 select e.`nomeEquipamento` as Equipamento, e.qtd as Quantidade from equipamento e
@@ -52,13 +52,14 @@ GROUP BY c.`nomeCliente`
 HAVING Total > 2;
 
 /*14. Listar a quantidade total do valor arrecadado nos aluguéis por tipo de equipamento.*/
-select e.`nomeEquipamento` as Equipamento, sum(a.`valorPago`) as Total from aluguel a
+select e.`nomeEquipamento` as Equipamento, sum(ae.`valorItem`) as 'Total Arrecadado' from aluguel a
 join aluguelequipamento ae on a.`idAluguel` = ae.`idAluguel`
 JOIN equipamento e ON ae.`idEquipamento` = e.`idEquipamento`
 GROUP BY e.`nomeEquipamento`;
 
 /*15. Mostrar o valor total movimentado por cliente*/
-select c.`nomeCliente` as Cliente, sum(`valorPago`) as 'Valor Pago' from aluguel a
+select c.`nomeCliente` as Cliente, sum(ae.`valorItem`) as 'Valor Pago' from aluguel a
+join aluguelequipamento ae on a.`idAluguel` = ae.`idAluguel`
 join cliente c on a.`idCliente` = c.`idCliente`
 GROUP BY c.`nomeCliente`;
 
@@ -73,13 +74,14 @@ GROUP BY f.`nomeFuncionario`
 ORDER BY COUNT(*) DESC LIMIT 1;
 
 /*18. Exibir a data dos aluguéis cujo valor total seja maior que R$ 100,00.*/
-select DATE_FORMAT(a.`dataHoraRetirada`, '%d/%m/%Y') as 'Data Aluguel', sum(a.`valorPago`) as Total from aluguelequipamento ae
+select DATE_FORMAT(a.`dataHoraRetirada`, '%d/%m/%Y') as 'Data Aluguel', sum(ae.`valorItem`) as Total from aluguelequipamento ae
 join aluguel a on ae.`idAluguel` = a.`idAluguel`
 GROUP BY ae.`idAluguel`
 HAVING Total > 100;
 
 /*19. Mostrar o total de valor pago por forma de pagamento.*/
-select a.`formaPagamento` as 'Forma de Pagamento', sum(a.`valorPago`) from aluguel a
+select a.`formaPagamento` as 'Forma de Pagamento', sum(ae.`valorItem`) from aluguel a
+join aluguelequipamento ae on a.`idAluguel` = ae.`idAluguel`
 WHERE a.`formaPagamento` is NOT null
 GROUP BY a.`formaPagamento`;
 
